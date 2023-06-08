f4b3e439c517ab43ffac64e878b97cc1d6c56895
LiaTauber9 <100442290+LiaTauber9@users.noreply.github.com>
2023-06-08 16:22:11 +0300

    download and contact

--- js/script.js
+++ js/script.js
@@ -62,3 +62,32 @@ const typed = new Typed('.multiple-text', {
     backDelay:1000,
     loop:true
 });
+
+
+/* ======================= typed js =======================*/
+function sendMail(event){
+    event.preventDefault();
+    console.log('sendMail()');
+    let params = {
+        name: document.getElementById('name').value,
+        email: document.getElementById('email').value,
+        phone: document.getElementById('phone').value,
+        subject: document.getElementById('subject').value,
+        message: document.getElementById('message').value
+    }
+
+    const serviceId = 'service_r04rkyh';
+    const templateId = 'template_qkhgn2x';
+
+    emailjs.send(serviceId,templateId,params)
+    .then(res=>{
+        document.getElementById('name').value = '';
+        document.getElementById('email').value = '';
+        document.getElementById('phone').value = '';
+        document.getElementById('subject').value = '';
+        document.getElementById('message').value ='';
+        console.log(res);
+        alert('Your message sent successfully')
+    })
+    .catch(err=>console.log(err))
+}
